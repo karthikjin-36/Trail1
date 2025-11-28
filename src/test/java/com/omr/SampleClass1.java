ffa84679b0c5fc6f89027ee282860fa3dc7d3fcb
karthikjin-36 <karthikreddragon@gmail.com>
2025-11-28 08:29:11 +0530

    Project addded first time in Repository

---
+++ src/test/java/com/omr/SampleClass1.java
@@ -0,0 +1,20 @@
+package com.omr;
+
+public class SampleClass1 {
+
+	private void method1() {
+		System.out.println("method 1 running...");
+	}
+	private void method2() {
+		System.out.println("method 2 running...");
+	}private void method3() {
+		System.out.println("method 3 running...");
+	}
+	public static void main(String[] args) {
+		SampleClass1 sampleClass1=new SampleClass1();
+		sampleClass1.method1();
+		sampleClass1.method2();
+		sampleClass1.method3();
+	}
+
+}
