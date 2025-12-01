3988ba449f1aca6256756c93cc3f1f7d242a4395
karthikjin-36 <karthikreddragon@gmail.com>
2025-12-01 18:07:48 +0530

    added method

--- src/test/java/com/omr/SampleClass1.java
+++ src/test/java/com/omr/SampleClass1.java
@@ -10,6 +10,9 @@ public class SampleClass1 {
 	}private void method3() {
 		System.out.println("method 3 running...");
 	}
+	private void method31() {
+		System.out.println("method 31");
+	}
 	public static void main(String[] args) {
 		SampleClass1 sampleClass1=new SampleClass1();
 		sampleClass1.method1();
